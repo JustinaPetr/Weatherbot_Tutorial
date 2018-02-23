491c207748e535413af4ea4617c922ed7a27e0fc
Bryan Kimani <bryankimani@users.noreply.github.com>
2018-02-23 22:56:03 +0300

    Update actions.py
    >  Have the imports from `__future__` in one line. This saves the number of lines to be written
    > Reduce Unnecessary line space between imports. see https://www.python.org/dev/peps/pep-0008/ for more info
    
    Great Job though

--- Full Code/actions.py
+++ Full Code/actions.py
@@ -1,7 +1,4 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import unicode_literals
-
+from __future__ import absolute_import, division, unicode_literals
 from rasa_core.actions.action import Action
 from rasa_core.events import SlotSet
 
